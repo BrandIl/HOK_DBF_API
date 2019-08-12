6c9d4abc3952830758b235ba379be9a567298a94
Sara Halili <4149065@gmail.com>
2019-08-12 16:48:56 +0300

    add cancel date

--- src/programs/program.reader.js
+++ src/programs/program.reader.js
@@ -1,6 +1,6 @@
 
 import dbfReader from '../common/dbfReader';
-
+import dateFormat from 'dateformat';
 const dbfMapping = {
     "PAYNUM": "key",
     "LAKNUM": "customerKey",
@@ -11,24 +11,35 @@ const dbfMapping = {
     "PAYSHEKEL": "sumShekel",
     "PAYDOLAR": "sumDollar",
     "PAYDESTENY": "projectKey",
-    "NORMALLATE": "collectionDate"
+    "NORMALLATE": "collectionDay",
+    "DATECANCEL": "cancelDate"
 }
 
 //PAYNUM	LAKNUM	PAYHESH	DATEJOIN	DATEOPEN	DATECLOSE	DATECANCEL	CANCELCOSE	PAYSHEKEL	PAYDOLAR	
 //MCHNUM	PAYCOSE	PAYDESTENY	PAYSTATUS	PAYREM	PAYNAME	PAYMAKAV	NORMALLATE	ISHURBANK	PAYPLACE	DATEMAKAV	PAYZACAUT2	PAYCITY	PAYKESHER
 
 
+const isEmptyDate = (date) => {
+    return dateFormat(new Date(date), "yyyy-mm-dd") === '2175-12-20';
+}
 
 const handleGetPrograms = records => {
     return records.map((program) => {
         const bankAccount = program.bankAccount && program.bankAccount.split(" ")
 
+
+        console.log(new Date(program.closeDate), dateFormat(new Date(program.closeDate), "yyyy-mm-dd"), isEmptyDate(program.closeDate));
         return Object.assign({}, program, {
             bankAccount: bankAccount && {
                 bank: bankAccount[0],
                 branch: bankAccount[1],
                 account: bankAccount[2]
-            }
+            },
+            cancelDate: isEmptyDate(program.cancelDate) ? undefined : new Date(program.cancelDate),
+            joinDate: isEmptyDate(program.joinDate) ? undefined : new Date(program.joinDate),
+            closeDate: isEmptyDate(program.closeDate) ? undefined : new Date(program.closeDate),
+            openDate: isEmptyDate(program.openDate) ? undefined : new Date(program.openDate),
+            collectionDay: Number(program.collectionDay)
         })
     });
 }
